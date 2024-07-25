aff36a174bea6d01d9c2d2621e67726a4acc7f84
kisia0916 <fumiharuabe@gmail.com>
2024-07-25 11:15:50 +0900

    change

--- src/apiinfo.ts
+++ src/apiinfo.ts
@@ -1,2 +1,2 @@
-// export const serverURL = "https://regic-server-main.onrender.com"
-export const serverURL = "http://localhost:5000"
+export const serverURL = "https://regic-server-main.onrender.com"
+// export const serverURL = "http://localhost:5000"
